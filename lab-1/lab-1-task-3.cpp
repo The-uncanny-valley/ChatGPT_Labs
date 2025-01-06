cd048d20a703c08a5d900b6c41216c952305efd7
Alina Andreeva <runcannyvalley@gmail.com>
2025-01-06 11:32:59 +0300

    std added

--- lab-1/lab-1-task-3.cpp
+++ lab-1/lab-1-task-3.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <cmath> // Для математических функций
+#include <cmath>
+using namespace std;
 
 int main() {
-    // Заданные значения
-    double x, y, z, a, b, c, s;
+    double x, y, z, a, b, c, s; // значения
     
-    std::cout << "x:";
-    std::cin >> x;
-    std::cout << "y:";
-    std::cin >> y;
-    std::cout << "z:";
-    std::cin >> z;
+    cout << "x:";
+    cin >> x;
+    cout << "y:";
+    cin >> y;
+    cout << "z:";
+    cin >> z;
 
     // Числитель первой части
     a = 1 + pow(sin(x + y), 2.0) * pow(x, fabs(y));
 
     // Знаменатель первой части
-    b = x - ((2 * y) / (1 + pow(x, 2.0) * pow(y, 2.0)));
-
+    b = fabs(x - ((2 * y) / (1 + pow(x, 2.0) * pow(y, 2.0))));
 
     // Вторая часть выражения
     c = pow(cos(atan(1.0 / z)), 2.0);
 
-    // Полное выражение
-    s = a / fabs(b) + c;
+    s = a / b + c;
 
-    // Вывод результата
-    std::cout << "Result s = " << s << std::endl;
+    cout << "Result s = " << s << endl;
 
     return 0;
 }
